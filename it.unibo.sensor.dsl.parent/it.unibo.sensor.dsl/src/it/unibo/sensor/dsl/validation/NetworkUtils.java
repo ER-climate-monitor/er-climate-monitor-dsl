2651e920a3bd244b752f50ec4bb31fa15a64b93d
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-05-14 10:13:43 +0200

    feat(util): create network utility class for automatically check if an ip is valid.

---
+++ it.unibo.sensor.dsl.parent/it.unibo.sensor.dsl/src/it/unibo/sensor/dsl/validation/NetworkUtils.java
@@ -0,0 +1,12 @@
+package it.unibo.sensor.dsl.validation;
+
+import java.util.regex.Matcher;
+
+public class NetworkUtils {
+    public static boolean isIpValid(final String ip) {
+        final String regexIpv4 = "^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\\.(?!$)|$)){4}$\n";
+        // source https://stackoverflow.com/questions/53497/regular-expression-that-matches-valid-ipv6-addresses
+        final String regexIpv6 = "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))";
+        return ip.matches(regexIpv4) || ip.matches(regexIpv6);
+    }
+}
